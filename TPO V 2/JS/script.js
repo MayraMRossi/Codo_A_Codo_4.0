cc76c6822be51e4ce8c76cc376c4207ea52c6bb5
Mayra Melina Rossi <81454006+MayraMRossi@users.noreply.github.com>
2021-10-18 18:10:28 -0300

    ok

--- TPO V 2/JS/script.js
+++
@@ -1,9 +0,0 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxeSb2vyBylGqsgbqbbeBSOWZo6NYtiulXPvKL7GFwPH-kc2r7D1TxZiHboPTNu11JI/exec'
-const form = document.forms['submit-form']
-
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message))
-})
