e0385c1bcd2a677818ba52629531ace2884a0dd0
Mayra Melina Rossi <mayra.rossi03@gmail.com>
2021-10-29 23:29:20 -0300

    ok

--- INCRUSTAR/JS/script.js
+++ INCRUSTAR/JS/script.js
@@ -1,12 +1,19 @@
 document.getElementById('email').addEventListener('input', function() {
-    campo =Event.target;
-    valido = document.getElementById('emailOK');
-        
-    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-    
-    if (emailRegex.test(campo.value)) {
-      valido.innerText = "válido";
-    } else {
-      valido.innerText = "incorrecto";
-    }
+  campo = event.target;
+  valido = document.getElementById('emailOK');
+      
+var reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+var regOficial = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+  //Se muestra un texto a modo de ejemplo, luego va a ser un icono
+  if (reg.test(campo.value) && regOficial.test(campo.value)) {
+    valido.innerText = "válido oficial y extraoficialmente";
+  } else if (reg.test(campo.value)) {
+    valido.innerText = "válido extraoficialmente";
+
+  } else {
+    valido.innerText = "incorrecto";
+
+}
 });
